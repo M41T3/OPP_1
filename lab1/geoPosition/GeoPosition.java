4b6e1cb4cf41216a5f5c07aaa035a7c6cb9d7b9b
maltemueller <maltemueller1104@gmail.com>
2019-04-22 09:40:07 +0200

    update.

--- lab1/geoPosition/GeoPosition.java
+++ lab1/geoPosition/GeoPosition.java
@@ -29,19 +29,23 @@ public class GeoPosition {
 	
 	public static double localDistanceInKm(GeoPosition a, GeoPosition b) {
 		double dy = 111.3 * Math.abs(a.latitude - b.latitude);
-		double dx = 111.3 * Math.cos((a.latitude + b.latitude)/2) * Math.abs(a.longitude - b.longitude);
+		double dx = 111.3 * Math.cos(Math.toRadians((a.latitude + b.latitude)/2.)) * Math.abs(a.longitude - b.longitude);
 		
 		return Math.sqrt(Math.pow(dx,2) + Math.pow(dy,2));
 	}
 	
 	public static double distanceInKm(GeoPosition a, GeoPosition b) {
 		
-		return 6378.388 * Math.acos(Math.sin(a.latitude) * Math.sin(b.latitude + Math.cos(a.latitude) * Math.cos(b.latitude) * Math.cos(b.longitude - a.longitude)));
+		return 6378.388 * Math.acos(Math.sin(Math.toRadians(a.latitude)) * Math.sin(Math.toRadians(b.latitude))
+				+ Math.cos(Math.toRadians(a.latitude)) * Math.cos(Math.toRadians(b.latitude)) * 
+				Math.cos(Math.toRadians(b.longitude - a.longitude)));
 	}
 
 	public double distanceInKm(GeoPosition other) {
 		
-		return 6378.388 * Math.acos(Math.sin(this.latitude) * Math.sin(other.latitude + Math.cos(this.latitude) * Math.cos(other.latitude) * Math.cos(other.longitude - this.longitude)));
+		return 6378.388 * Math.acos(Math.sin(Math.toRadians(this.latitude)) * Math.sin(Math.toRadians(other.latitude))
+				+ Math.cos(Math.toRadians(this.latitude)) * Math.cos(Math.toRadians(other.latitude)) 
+				* Math.cos(Math.toRadians(other.longitude - this.longitude)));
 	}
 
 	@Override
